78a3aec9658611fe557a19063993618191279300
xexeu-sjc <69054130+xexeu-sjc@users.noreply.github.com>
2021-05-08 07:35:48 -0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+led.enable(false)
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    basic.pause(1000)
+})
